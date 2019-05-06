0152c10b899939b1440b5f3e043ee8cfe9a1d15a
Elvis Pestana <elvispt@gmail.com>
2019-05-06 15:50:55 +0100

    Use home controller

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::namespace('Notes')
     ->prefix('notes')
