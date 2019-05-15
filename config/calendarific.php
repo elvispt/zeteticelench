1087bb6e0101e692ed928f43f566c3d9544b3d23
Elvis Pestana <elvispt@gmail.com>
2019-05-15 16:57:46 +0100

    Show next holiday on dashboard
    Closes #47

---
+++ config/calendarific.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'base_uri' => 'https://calendarific.com',
+    'holidays_endpoint' => '/api/v2/holidays',
+    'api_key' => env('CALENDARIFIC_API_KEY'),
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,8 @@
 @section('content')
   <div class="container">
     <div class="row justify-content-center">
-      <div class="col-md-8">
+
+      <div class="col-sm-6">
         <div class="card">
           <div class="card-header">Dashboard</div>
 
@@ -18,6 +19,31 @@
           </div>
         </div>
       </div>
+
+      <div class="col-sm-6">
+        <div class="card">
+          <div class="card-header">Next Holiday</div>
+          <div class="card-body">
+            @foreach($nextHolidays as $nextHoliday)
+              <p>
+                <span class="badge badge-dark">#</span>
+                {{ $nextHoliday->name }},
+                {{ \Illuminate\Support\Carbon::make($nextHoliday->date->iso)->diffForHumans() }}
+                at {{ \Illuminate\Support\Carbon::make($nextHoliday->date->iso)->format('Y-m-d') }}
+                @foreach ($nextHoliday->type as $tp)
+                  <small>{{ $tp }}</small>
+                @endforeach
+              </p>
+              <p>
+                <small>{{ $nextHoliday->description }}</small>
+
+              </p>
+
+            @endforeach
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 @endsection
