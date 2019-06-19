93dddc705a79b3d96579e217b091c4699d52d4b9
Elvis Pestana <elvispt@gmail.com>
2019-06-19 17:49:50 +0100

    Test command StaleTags
    Ref: #103

---
+++ tests/Feature/StaleTagsCommandTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Carbon;
+use Tests\TestCase;
+
+class StaleTagsCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testRemoveStaleTags()
+    {
+        $this->createTestTags();
+        $this->artisan('tags:stale')
+             ->expectsQuestion('This is a destructive command. Continue?', true)
+             ->expectsOutput('Removed 6 stale tags.')
+             ->assertExitCode(0);
+    }
+
+    public function testRemoveStaleTagsNoTagsOnDatabase()
+    {
+        $this->artisan('tags:stale')
+             ->expectsQuestion('This is a destructive command. Continue?', true)
+             ->expectsOutput('Removed 0 stale tags.')
+             ->assertExitCode(0);
+    }
+
+    public function testRemoveStaleTagsNoConfirmation()
+    {
+        $this->artisan('tags:stale')
+             ->expectsQuestion('This is a destructive command. Continue?', false)
+             ->expectsOutput('Canceled!')
+             ->assertExitCode(0);
+    }
+
+    /**
+     * Will create 6 tags that can be removed.
+     */
+    protected function createTestTags()
+    {
+        factory(User::class, 3)
+            ->create([
+                'created_at' => Carbon::now()->addWeeks(-10),
+            ])
+            ->each(function (User $user) {
+                $user->tags()->saveMany(factory(Tag::class, 2)->make());
+            });
+    }
+}
