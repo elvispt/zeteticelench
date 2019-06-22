b0fc122cd1f2cf4b4e112a6bbdafd35520aa4701
Elvis Pestana <elvispt@gmail.com>
2019-06-22 11:40:11 +0100

    Add test for StoreItems
    This test checks for number of changed items.
    Changed items are the updated/created/failed items.
    Ref: #103

--- tests/Feature/HackerNews/StoreItemsTest.php
+++ tests/Feature/HackerNews/StoreItemsTest.php
@@ -13,7 +13,7 @@ class StoreItemsTest extends TestCase
     public function testStoreStories()
     {
         $stories = [
-            // invalid
+            // valid
             (object) [
                 'id' => 5,
                 'type' => 'story',
@@ -71,7 +71,7 @@ class StoreItemsTest extends TestCase
     public function testStoreStoriesNoValidStories()
     {
         $stories = [
-            // invalid
+            // valid
             (object) [
                 'id' => 5,
                 'type' => 'story23',
@@ -114,5 +114,81 @@ class StoreItemsTest extends TestCase
         $storeItems = new StoreItems();
         $nStories = $storeItems->store($stories);
         $this->assertEquals(0, $nStories);
+    }
+
+    public function testStoreStoriesChangedItems()
+    {
+        $stories = [
+            // valid
+            (object) [
+                'id' => 5,
+                'type' => 'story',
+            ],
+            // invalid
+            (object) [
+                'id' => 6,
+                'parent' => 123,
+                'kids' => [242, 424, 242, 484, 328],
+                'dead' => true,
+                'url' => 'http://www.googl.ecom',
+                'score' => 12416,
+                'title' => 'this is a title',
+                'text' => 'this is the text',
+            ],
+            // valid
+            (object) [
+                'id' => 6,
+                'parent' => 123,
+                'kids' => [242, 424, 242, 484, 328],
+                'dead' => false,
+                'url' => 'http://www.googl.ecom',
+                'score' => 351,
+                'title' => 'this is a title',
+                'text' => 'this is the text',
+                'time' => 98745456,
+                'by' => 'somerandom',
+                'descendants' => 8,
+                'type' => 'story',
+                'deleted' => true,
+            ],
+            // invalid
+            (object) [
+                'id' => 79,
+                'type' => 'WTF',
+            ],
+            // valid
+            (object) [
+                'id' => 6,
+                'parent' => 321,
+                'kids' => [242, 484, 328],
+                'dead' => false,
+                'url' => 'http://wow',
+                'score' => 351,
+                'title' => 'this is a title',
+                'text' => 'this is the text',
+                //
+                'time' => 98745456,
+                'by' => 'somerandom',
+                'type' => 'story',
+                'deleted' => true,
+            ],
+        ];
+
+        $storeItems = new StoreItems();
+        $nStories = $storeItems->store($stories);
+        $this->assertEquals(3, $nStories);
+        $this->assertDatabaseHas('hacker_news_items', [
+            'id' => 6,
+            'parent_id' => 321,
+            'text' => 'this is the text',
+            'by' => 'somerandom',
+        ]);
+        $this->assertDatabaseHas('hacker_news_items', [
+            'id' => 5,
+            'type' => 'story',
+        ]);
+        $changed = $storeItems->getChanges();
+        $this->assertEquals(2, $changed['new']);
+        $this->assertEquals(1, $changed['updated']);
     }
 }
