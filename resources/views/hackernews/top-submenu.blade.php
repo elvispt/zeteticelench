6591632f5f126d7f7b0354f8273c7c5c8d430d3f
Elvis Pestana <elvispt@gmail.com>
2019-07-02 14:42:11 +0100

    Add custom blade directive to topsubmenu

--- resources/views/hackernews/top-submenu.blade.php
+++ resources/views/hackernews/top-submenu.blade.php
@@ -1,25 +1,24 @@
 <div class="row justify-content-center top-submenu">
   <div class="col-12 text-center">
     <div class="btn-group d-flex mb-2">
-      @php $currentRouteName = Route::currentRouteName(); @endphp
       <a
-        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-top' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 @submenuactive('hackernews-top')"
         href="{{ route('hackernews-top') }}"
       >@lang('hackernews.top')</a>
       <a
-        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-best' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 @submenuactive('hackernews-best')"
         href="{{ route('hackernews-best') }}"
       >@lang('hackernews.best')</a>
       <a
-        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-new' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 @submenuactive('hackernews-new')"
         href="{{ route('hackernews-new') }}"
       >@lang('hackernews.new')</a>
       <a
-        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-bookmark-list' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 @submenuactive('hackernews-bookmark-list')"
         href="{{ route('hackernews-bookmark-list') }}"
       >@lang('hackernews.bookmarked')</a>
       <a
-        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-jobs' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 @submenuactive('jobs')"
         href="{{ route('hackernews-jobs') }}"
       >@lang('hackernews.jobs')</a>
     </div>
