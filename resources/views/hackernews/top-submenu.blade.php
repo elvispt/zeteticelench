1cf50b96809af88cdad3a8832de23decaf392a61
Elvis Pestana <elvispt@gmail.com>
2019-07-02 14:03:33 +0100

    Submenu should use 100% of horizontal space

--- resources/views/hackernews/top-submenu.blade.php
+++ resources/views/hackernews/top-submenu.blade.php
@@ -1,25 +1,25 @@
 <div class="row justify-content-center top-submenu">
   <div class="col-12 text-center">
-    <div class="btn-group mb-2">
+    <div class="btn-group d-flex mb-2">
       @php $currentRouteName = Route::currentRouteName(); @endphp
       <a
-        class="btn btn-group-sm {{ $currentRouteName === 'hackernews-top' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-top' ? 'btn-primary' : 'btn-secondary'}}"
         href="{{ route('hackernews-top') }}"
       >@lang('hackernews.top')</a>
       <a
-        class="btn btn-group-sm {{ $currentRouteName === 'hackernews-best' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-best' ? 'btn-primary' : 'btn-secondary'}}"
         href="{{ route('hackernews-best') }}"
       >@lang('hackernews.best')</a>
       <a
-        class="btn btn-group-sm {{ $currentRouteName === 'hackernews-new' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-new' ? 'btn-primary' : 'btn-secondary'}}"
         href="{{ route('hackernews-new') }}"
       >@lang('hackernews.new')</a>
       <a
-        class="btn btn-group-sm {{ $currentRouteName === 'hackernews-bookmark-list' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-bookmark-list' ? 'btn-primary' : 'btn-secondary'}}"
         href="{{ route('hackernews-bookmark-list') }}"
       >@lang('hackernews.bookmarked')</a>
       <a
-        class="btn btn-group-sm {{ $currentRouteName === 'hackernews-jobs' ? 'btn-primary' : 'btn-secondary'}}"
+        class="btn btn-group-sm w-100 {{ $currentRouteName === 'hackernews-jobs' ? 'btn-primary' : 'btn-secondary'}}"
         href="{{ route('hackernews-jobs') }}"
       >@lang('hackernews.jobs')</a>
     </div>
