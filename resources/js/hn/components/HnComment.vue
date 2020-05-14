ef073df54deb34b5b56c5ab46594e01008440c20
Elvis Pestana <elvispt@gmail.com>
2020-05-14 15:07:16 +0100

    Add comment collapsed on hn comments

--- resources/js/hn/components/HnComment.vue
+++ resources/js/hn/components/HnComment.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="hn-comment card mt-3">
     <div class="card-body px-2 px-md-3 shadow">
-      <h6 class="card-subtitle bg-light text-muted d-flex justify-content-between align-items-center">
+      <h6 class="card-subtitle bg-light text-muted d-flex justify-content-between align-items-center"
+          :class="{ 'opacity-5 text-line-through': comment.deleted }"
+      >
         <span class="align-middle">by {{ comment.by }}</span>
         <small class="text-muted">{{ comment.time | diffForHumans }}</small>
+        <a role="button"
+           class="btn btn-sm btn-outline-secondary pointer"
+           data-toggle="collapse"
+           @click="isShow = !isShow"
+        >↕️️</a>
       </h6>
 
-      <div class="mt-2">
-        <p v-html="comment.text"></p>
-        <hn-comment
-          v-if="comment.comments.length"
-          v-for="com in comment.comments"
-          v-bind:key="com.id"
-          :comment="com"
-        ></hn-comment>
-      </div>
+      <transition name="slide">
+        <div class="mt-2" v-if="isShow">
+          <p v-html="comment.text"></p>
+          <hn-comment
+            v-if="comment.comments.length"
+            v-for="com in comment.comments"
+            v-bind:key="com.id"
+            :comment="com"
+          ></hn-comment>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -26,5 +35,48 @@ export default {
   props: {
     comment: { type: Object, required: true },
   },
+
+  data() {
+    return {
+      isShow: true,
+    };
+  },
 }
 </script>
+
+<style scoped>
+  .slide-enter-active {
+    -moz-transition-duration: 0.3s;
+    -webkit-transition-duration: 0.3s;
+    -o-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -moz-transition-timing-function: ease-in;
+    -webkit-transition-timing-function: ease-in;
+    -o-transition-timing-function: ease-in;
+    transition-timing-function: ease-in;
+  }
+
+  .slide-leave-active {
+    -moz-transition-duration: 0.3s;
+    -webkit-transition-duration: 0.3s;
+    -o-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -moz-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+    -webkit-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+    -o-transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+    transition-timing-function: cubic-bezier(0, 1, 0.5, 1);
+  }
+
+  .slide-enter-to, .slide-leave {
+    max-height: 1000px;
+    overflow: hidden;
+  }
+
+  .slide-enter, .slide-leave-to {
+    overflow: hidden;
+    max-height: 0;
+  }
+  .text-line-through {
+    text-decoration: line-through;
+  }
+</style>
