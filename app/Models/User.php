d9b3c69e41b4dc0150947dc00f63086980d7a517
Elvis Pestana <elvispt@gmail.com>
2021-08-04 13:15:45 +0100

    Update Laravel framework to v8

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -56,7 +58,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
